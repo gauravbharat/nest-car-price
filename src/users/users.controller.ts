72ce2e78bbf471014ad6ff8cc4c27b48f244adde
Gaurav Bharat <gaurav.mendse@gmail.com>
2026-01-30 15:38:02 +0530

    inject user repo; create and save user

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,10 +1,13 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
+import { UsersService } from './users.service';
 
 @Controller('auth')
 export class UsersController {
+  constructor(private readonly userService: UsersService) {}
+
   @Post('/signup')
   createUser(@Body() body: CreateUserDto) {
-    console.log('body', body);
+    return this.userService.create(body.email, body.password);
   }
 }
