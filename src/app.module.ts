e049a951471d199c912ff8b3951a39e8669fd2aa
Gaurav Bharat <gaurav.mendse@gmail.com>
2026-02-05 20:54:20 +0530

    use nest config module to configure env variables and test vs dev database; used cross env to set process node env variable

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,5 @@
 import { MiddlewareConsumer, Module, ValidationPipe } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
@@ -24,18 +25,27 @@ const cookieSession = require('cookie-session');
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'sqlite',
-      database: 'db.sqlite',
-      entities: [User, Report],
-      /** DEV ONLY OPTION! NOT FOR PROD SINCE DB SCHEMA CAN BE AUTO-CREATED ON EACH APP LAUNCH!!
-       * When an Entity is updated, this flag if TRUE, lets typeORM to update the DB schema.
-       * It can DELETE a column as well if removed from Entity!!
-       * Use cautiously and for initial DB structure only when in still in DEVELOPMENT!!
-       *
-       * If this flag is FALSE, then a migration SQL script has to be created to update/change the DB structure manually.
-       */
-      synchronize: true,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: `.env.${process.env.NODE_ENV}`,
+    }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => {
+        return {
+          type: 'sqlite',
+          database: config.get<string>('DB_NAME'),
+          entities: [User, Report],
+          /** DEV ONLY OPTION! NOT FOR PROD SINCE DB SCHEMA CAN BE AUTO-CREATED ON EACH APP LAUNCH!!
+           * When an Entity is updated, this flag if TRUE, lets typeORM to update the DB schema.
+           * It can DELETE a column as well if removed from Entity!!
+           * Use cautiously and for initial DB structure only when in still in DEVELOPMENT!!
+           *
+           * If this flag is FALSE, then a migration SQL script has to be created to update/change the DB structure manually.
+           */
+          synchronize: true,
+        };
+      },
     }),
     UsersModule,
     ReportsModule,
